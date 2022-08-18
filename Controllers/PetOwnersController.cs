53ac56de19c5e0d4d85f185e6f2a045a3491f48d
John Maras <sportsballenthusiast@gmail.com>
2022-08-17 21:17:27 -0500

    I'm so lost

--- Controllers/PetOwnersController.cs
+++ Controllers/PetOwnersController.cs
@@ -12,14 +12,17 @@ namespace pet_hotel.Controllers
     public class PetOwnersController : ControllerBase
     {
         private readonly ApplicationContext _context;
-        public PetOwnersController(ApplicationContext context) {
+        public PetOwnersController(ApplicationContext context)
+        {
             _context = context;
         }
 
         // This is just a stub for GET / to prevent any weird frontend errors that 
         // occur when the route is missing in this controller
         [HttpGet]
-        public IEnumerable<PetOwner> GetPets() {
+        public IEnumerable<PetOwner> GetPets()
+        {
+            Console.WriteLine("get the pet owners, baby");
             return new List<PetOwner>();
         }
     }
--- Models/Pet.cs
+++ Models/Pet.cs
@@ -5,7 +5,29 @@ using System;
 
 namespace pet_hotel
 {
-    public enum PetBreedType {}
-    public enum PetColorType {}
-    public class Pet {}
+    public enum PetBreedType
+    {
+        Shepherd, Poodle, Beagle, Bulldog, Terrier, Boxer, Labrador, Retriever,
+    }
+    public enum PetColorType
+    {
+        White, Black, Golden, Tricolor, Spotted
+    }
+    public class Pet
+    {
+        [Required]
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+        [Required]
+        public PetColorType color { get; set; }
+        [Required]
+        public PetBreedType breed { get; set; }
+
+        public DateTime checkedInAt { get; set; }
+        [ForeignKey("PetOwner")]
+        public int PetOwnerId { get; set; }
+        public virtual PetOwner petOwner { get; set; }
+
+    }
 }
